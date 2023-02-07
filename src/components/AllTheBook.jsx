bdc4f4730fb678c1f6d477ee23a7e8ec3a548a4e
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-02-07 17:07:36 +0100

    add welcome and navbar

--- src/components/AllTheBook.jsx
+++ src/components/AllTheBook.jsx
@@ -1,20 +1,24 @@
 
-import {Card, Button} from 'react-bootstrap';
+import {Card, Button,} from 'react-bootstrap';
+import horror from "../arc/horror.json"
 
-function BasicExample() {
+function Books()
+ {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-}
+    {horror.map((book)=>{
+      return(    <Card style={{ width: '18rem' }} key={horror.asin}>
+      
 
-export default BasicExample;
+      <Card.Img variant="top" src={horror.img} />
+      <Card.Body>
+        <Card.Title>{horror.title}</Card.Title>
+        <Card.Text>{horror.price}</Card.Text>
+        <Button variant="primary d-flex justify-content-center">Order</Button>
+      </Card.Body>
+    </Card>)})
+    })}
+
+  ;
+
+    
+export default Books;
